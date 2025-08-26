9d6b7760848947960fc269b6fce5bc3e3b34033c
Rafael Nunes <rafalex.fox@gmail.com>
2025-08-26 15:51:52 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <div className="header">
+            menu superior
+            fundo
+            logo
+        </div>
+
+        <div className="chamada">
+            Catsitters veterinárias em São Paulo, SP
+                Cuidado profissional e carinhoso para gatinhos no conforto da sua casa — visitas agendadas, relatórios com fotos e suporte de quem entende de saúde felina.
+
+                100% domiciliar
+                Relatório com fotos
+                Medicação oral/tópica
+                Profissionais veterinárias
+                Agendar agora
+                Ver planos
+        </div>
+
+        <div className="servicos">
+            o que esta incluso
+        </div>
+
+        <div className="mapa">
+            onde atendemos
+        </div>
+
+        <div className="plano">
+            planos
+        </div>
+
+        <div className="depoimentos">
+            depoimentos
+        </div>
+
+        <div className="perguntas">
+
+        </div>
+
+        <div className="contato">
+            
+        </div>
+    </>
+  )
+}
+
+export default App
